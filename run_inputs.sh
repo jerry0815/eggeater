6015b14bbc7a6f42ae47d1e2af64e2a33bf3bc3c
jerry <linjerry890815@gmail.com>
2023-05-23 11:57:41 -0700

    add run inputs script

--- run_inputs.sh
+++ run_inputs.sh
@@ -1,7 +1,7 @@
 
 for filename in ./inputs/*.snek; do
     file=$(basename $filename .snek)
-    cargo run -- $file.snek inputs/$file.s
+    cargo run -- ./inputs/$file.snek ./inputs/$file.s
     nasm -f elf64 ./inputs/$file.s -o ./inputs/$file.o
     ar rcs ./inputs/lib$file.a ./inputs/$file.o
     rustc -L ./inputs/ -lour_code:$file ./runtime/start.rs -o ./inputs/$file.run
